89416a52a9588247b549c0faa0d29cb1f9c67f99
Dalhros <arcimedeslas@yahoo.com>
2015-04-10 15:38:49 -0400

    extra stuff

---
+++ MolComSim/src/IntermediateNodeParam.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class IntermediateNodeParam extends NanoMachineParam {
+	private Position infoMolReleasePoint;
+	private Position ackMolReleasePoint;
+	
+	public IntermediateNodeParam(Position cntr, double r, Position infoPoint, Position ackPoint) {
+		super(cntr, r, cntr);
+		infoMolReleasePoint = infoPoint;
+		ackMolReleasePoint = ackPoint;
+	}
+	
+	public IntermediateNodeParam(Scanner readParams) {
+		super(readParams);
+		infoMolReleasePoint =  super.getMolReleasePoint();
+		if(readParams.hasNext()) {
+			ackMolReleasePoint = new Position(readParams);
+		} else {
+			ackMolReleasePoint = new Position(super.getCenter().getX(), super.getCenter().getY(), super.getCenter().getZ());
+		}
+	}
+
+	public Position getInfoMolReleasePoint() {
+		return infoMolReleasePoint;
+	}
+	
+	public Position getAckMolReleasePoint() {
+		return ackMolReleasePoint;
+	}
+	
+	
+}
