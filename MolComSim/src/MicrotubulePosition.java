b30f01913bf1abe83e6652d22d09b5bd1b696142
Bria Morgan <calypsomatic@gmail.com>
2015-04-11 15:17:26 -0400

    g

---
+++ MolComSim/src/MicrotubulePosition.java
@@ -0,0 +1,19 @@
+public class MicrotubulePosition {
+
+	Position plusEndPoint;
+	Position minusEndPoint;
+	
+	public MicrotubulePosition(Position plus, Position minus){
+		this.plusEndPoint = plus;
+		this.minusEndPoint = minus;
+	}
+	
+	public Position getPlusEndPoint(){
+		return this.plusEndPoint;
+	}
+	
+	public Position getMinusEndPoint(){
+		return this.minusEndPoint;
+	}
+
+}
